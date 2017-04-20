d72e1510d1814496875341caacdd6147e7df7173
Terence Chen <chenjienan2009@gmail.com>
2017-04-20 21:59:55 +0000

    integrate with slack

---
+++ config.py
@@ -0,0 +1,2 @@
+SLACK_URL = "https://ssqjqfgkj7.execute-api.us-west-2.amazonaws.com/prod"
+JSON_HEADER = {'Content-type': 'application/json'}
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from Tkinter import *
 import tkMessageBox
+import slack
 
 
 class IdleMode:
@@ -9,33 +10,96 @@ class IdleMode:
     def __init__(self, master):
         self.master = master
         self.frame = Frame(self.master)
-        self.btn_1 = Button(self.frame, text="Need 1", height=20, width=15)
-        self.btn_2 = Button(self.frame, text="Need 2", height=20, width=15)
-        self.btn_3 = Button(self.frame, text="Need 3", height=20, width=15)
-        self.start_btn = Button(self.frame, text="START", bg="green", height=20, width=15, command=self.go_to_game)
-        self.done_btn = Button(self.frame, text="DONE", bg="red", height=20, width=15)
+
+        self.btn_1 = Button(
+            self.frame,
+            text="1",
+            font=(None, 80),
+            height=13,
+            width=2,
+            command=self.need_1_player
+        )
+        
+        self.btn_2 = Button(
+            self.frame,
+            text="2",
+            font=(None, 80),
+            height=13,
+            width=2,
+            command=self.need_2_players
+        )
+        
+        self.btn_3 = Button(
+            self.frame,
+            text="3",
+            font=(None, 80),
+            height=13,
+            width=2,
+            command=self.need_3_players
+        )
+        
+        self.done_btn = Button(
+            self.frame,
+            text="DONE",
+            bg="red",
+            fg="white",
+            activeforeground="white",
+            activebackground="red",
+            height=13,
+            width=16,
+            font=(None, 10))
+        
+        self.start_btn = Button(
+            self.frame,
+            text="START",
+            bg="green",
+            
+            fg="white",
+            activeforeground="white",
+            activebackground="green",
+            height=13,
+            width=16,
+            font=(None, 10),
+            command=self.go_to_game)
 
         self.btn_1.pack(side=LEFT)
         self.btn_2.pack(side=LEFT)
         self.btn_3.pack(side=LEFT)
-        self.start_btn.pack(side=LEFT)
         self.done_btn.pack(side=LEFT)
+        self.start_btn.pack(side=LEFT)
 
-        self.label = Label(self.master,
-                           bg="blue",
-                           fg="black",
-                           text="How many player do you need?",
-                           height=5,
-                           width=50,
-                           font=(None, 20))
+        self.top_label = Label(self.master,
+                               bg="blue",
+                               fg="white",
+                               text="Players needed",
+                               height=3,
+                               width=50,
+                               font=(None, 20))
 
-        self.label.pack()
+        self.bottom_label = Label(self.master,
+                                  height=2,
+                                  width=50,
+                                  text = "test",
+                                  font=(None, 10))
+
+        self.top_label.pack()
         self.frame.pack()
+        self.bottom_label.pack()
 
     def go_to_game(self):
         self.new_window = Toplevel(self.master)
         self.new_window.attributes("-fullscreen", True)
         self.app = GameMode(self.new_window)
+
+    def need_1_player(self):
+        slack.send_message("Need 1 player")
+        
+    def need_2_players(self):
+        slack.send_message("Need 2 players")
+        
+    def need_3_players(self):
+        slack.send_message("Need 3 players")
+        
         
         
 class GameMode:
@@ -47,7 +111,7 @@ class GameMode:
     
         self.label = Label(self.master,
                            bg="red",
-                           fg="black",
+                           fg="white",
                            text="We are in the Game!",
                            height=5,
                            width=50,
