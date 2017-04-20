5623123c75ef1199643a461c570506fe9347f259
Terence Chen <chenjienan2009@gmail.com>
2017-04-20 18:51:57 +0000

    change button and label size

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from Tkinter import *
 window = Tk()
 frame = Frame(window)
 
-btn_1 = Button(frame, text="Need 1")
-btn_2 = Button(frame, text="Need 2")
-btn_3 = Button(frame, text="Need 3")
-btn_4 = Button(frame, text="START")
-btn_5 = Button(frame, text="DONE")
+btn_1 = Button(frame, text="Need 1", height=20, width=15)
+btn_2 = Button(frame, text="Need 2", height=20, width=15)
+btn_3 = Button(frame, text="Need 3", height=20, width=15)
+btn_4 = Button(frame, text="START", bg="green", height=20, width=15)
+btn_5 = Button(frame, text="DONE", bg="red", height=20, width=15)
 
 btn_1.pack(side=LEFT)
 btn_2.pack(side=LEFT)
@@ -18,7 +18,13 @@ btn_3.pack(side=LEFT)
 btn_4.pack(side=LEFT)
 btn_5.pack(side=LEFT)
 
-label = Label(window, text="How many player do you need?")
+label = Label(window,
+              bg="blue",
+              fg="black",
+              text="How many player do you need?",
+              height=5,
+              width=50,
+              font=(None, 20))
 label.pack()
 frame.pack()
 
