aaba0611f40f25c0da4be8605147c3039281c4b1
Kosmin <innorogue@gmail.com>
2017-04-21 17:56:30 +0000

    testing the video recording

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
+import os
+
+
 SLACK_URL = "https://ssqjqfgkj7.execute-api.us-west-2.amazonaws.com/prod"
 JSON_HEADER = {'Content-type': 'application/json'}
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+video_name = 'latest.h264'
+video_path = os.path.join(current_path, video_name)
